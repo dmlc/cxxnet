f669b82b79f345ff6254bded60acef655068d3e1
antinucleon <antinucleon@gmail.com>
2014-03-07 14:09:19 -0700

    new init

---
+++ cxxnet/cxxnet.h
@@ -0,0 +1,18 @@
+#ifndef CXXNET_H
+#define CXXNET_H
+#pragma once
+/*!
+ * \brief Base definition for cxxnet
+ * \author Bing Xu
+ */
+
+#include "mshadow/tensor.h"
+namespace cxxnet {
+
+typedef mshadow::cpu cpu;
+typedef mshadow::gpu gpu;
+
+};
+
+
+#endif // CXXNET_H
