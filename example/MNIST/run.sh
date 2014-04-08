02fdc041c58d50e3bc41284df2916e3ea91c0c80
antinucleon <antinucleon@gmail.com>
2014-04-08 11:40:19 -0600

    run.sh

---
+++ example/MNIST/run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [ ! -d "data" ]; then
+    mkdir data
+fi
+
+cd data
+
+if [ ! -f "train-images-idx3-ubyte.gz" ]; then
+    wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+fi
+
+if [ ! -f "train-labels-idx1-ubyte.gz" ]; then
+    wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+fi
+
+if [ ! -f "t10k-images-idx3-ubyte.gz" ]; then
+    wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+fi
+
+if [ ! -f "t10k-labels-idx1-ubyte.gz" ]; then
+    wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+fi
+
+cd ..
+
+if [ ! -d "models" ]; then
+    mkdir models
+fi
+
+../../cxxnet_learner $1
