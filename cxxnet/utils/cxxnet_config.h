45553ea0e36f1e67317e67a319968669aaea9910
tqchen <tqchen@graphlab.com>
2014-07-13 18:41:20 +0000

    make config reader more flexible, allow using istream such as directly parse from string

--- cxxnet/utils/cxxnet_config.h
+++ cxxnet/utils/cxxnet_config.h
@@ -9,26 +9,16 @@
 #include <cstring>
 #include <string>
 #include "cxxnet_utils.h"
+#include <istream>
+#include <fstream>
 
 namespace cxxnet{
     namespace utils{
         /*! 
-         * \brief an iterator that iterates over a configure file and gets the configures
+         * \brief base implementation of config reader
          */
-        class ConfigIterator{
-        public:
-            /*! 
-             * \brief constructor 
-             * \param fname name of configure file
-             */
-            ConfigIterator( const char *fname ){
-                fi = FopenCheck( fname, "r");
-                ch_buf = fgetc( fi );
-            }
-            /*! \brief destructor */
-            ~ConfigIterator(){
-                fclose( fi );
-            }
+        class ConfigReaderBase{
+	public:
             /*! 
              * \brief get current name, called after Next returns true
              * \return current parameter name 
@@ -48,7 +38,7 @@ namespace cxxnet{
              * \return true if there is value in next position
              */
             inline bool Next( void ){
-                while( !feof( fi ) ){
+	        while( !this->IsEnd()){
                     GetNextToken( s_name );
                     if( s_name[0] == '=')  return false;               
                     if( GetNextToken( s_buf ) || s_buf[0] != '=' ) return false;
@@ -57,22 +47,30 @@ namespace cxxnet{
                 }
                 return false;
             }
-        private:
-            FILE *fi;        
+	    // called before usage
+	    inline void Init(void){
+                ch_buf = this->GetChar();
+	    }
+	protected:
+	    /*! \brief to be implemented by child, get next token, return EOF if end of file*/
+  	    virtual char GetChar(void) = 0;
+	    /*! \brief to be implemented by child, check if end of stream*/
+	    virtual bool IsEnd(void)=0;
+	private:
             char ch_buf;
             char s_name[100000],s_val[100000],s_buf[100000];
             
             inline void SkipLine(){           
                 do{
-                    ch_buf = fgetc( fi );
+		    ch_buf = this->GetChar();
                 }while( ch_buf != EOF && ch_buf != '\n' && ch_buf != '\r' );
             }
             
             inline void ParseStr( char tok[] ){
                 int i = 0; 
-                while( (ch_buf = fgetc(fi)) != EOF ){
+                while( (ch_buf = this->GetChar()) != EOF ){
                     switch( ch_buf ){
-                    case '\\': tok[i++] = fgetc( fi ); break;
+                    case '\\': tok[i++] = this->GetChar(); break;
                     case '\"': tok[i++] = '\0'; return;
                     case '\r':
                     case '\n': Error("unterminated string"); 
@@ -84,9 +82,9 @@ namespace cxxnet{
 
             inline void ParseStrML( char tok[] ){
                 int i = 0; 
-                while( (ch_buf = fgetc(fi)) != EOF ){
+                while( (ch_buf = this->GetChar()) != EOF ){
                     switch( ch_buf ){
-                    case '\\': tok[i++] = fgetc( fi ); break;
+                    case '\\': tok[i++] = this->GetChar(); break;
                     case '\'': tok[i++] = '\0'; return;
                     default: tok[i++] = ch_buf;
                     }
@@ -102,31 +100,31 @@ namespace cxxnet{
                     case '#' : SkipLine(); new_line = true; break;
                     case '\"':
                         if( i == 0 ){
-                            ParseStr( tok );ch_buf = fgetc(fi); return new_line;
+			    ParseStr( tok );ch_buf = this->GetChar(); return new_line;
                         }else{
                             Error("token followed directly by string"); 
                         }
                     case '\'':
                         if( i == 0 ){
-                            ParseStrML( tok );ch_buf = fgetc(fi); return new_line;
+			    ParseStrML( tok );ch_buf = this->GetChar(); return new_line;
                         }else{
                             Error("token followed directly by string"); 
                         }
                     case '=':
                         if( i == 0 ) {
-                            ch_buf = fgetc( fi );     
+                            ch_buf = this->GetChar();     
                             tok[0] = '='; 
                             tok[1] = '\0'; 
                         }else{
                             tok[i] = '\0'; 
                         }
                         return new_line;
-                case '\r':
+		    case '\r':
                     case '\n':
-					if( i == 0 ) new_line = true;
+		      if( i == 0 ) new_line = true;
                     case '\t':
                     case ' ' :
-                        ch_buf = fgetc( fi );
+                        ch_buf = this->GetChar();
                         if( i > 0 ){
                             tok[i] = '\0'; 
                             return new_line;
@@ -134,12 +132,61 @@ namespace cxxnet{
                         break;
                     default: 
                         tok[i++] = ch_buf;
-                        ch_buf = fgetc( fi );
+                        ch_buf = this->GetChar();
                         break;                    
                     }
                 }
                 return true;
             }
+
+	};
+      
+        /*! 
+         * \brief an iterator use stream base, allows use all types of istream
+         */
+        class ConfigStreamReader: public ConfigReaderBase{
+        public:
+            /*! 
+             * \brief constructor 
+             * \param istream input stream 
+             */
+	    ConfigStreamReader( std::istream &fin ):fin(fin){}
+	protected:
+	    /*! \brief to be implemented by child, get next token, return EOF if end of file */
+	    virtual char GetChar(void){
+	        return fin.get();
+	    }
+	    /*! \brief to be implemented by child, check if end of stream */
+	    virtual bool IsEnd(void){
+	        return fin.eof();
+	    }
+	private:
+	    std::istream &fin;
+        };
+
+        /*! 
+         * \brief an iterator that iterates over a configure file and gets the configures
+         */
+        class ConfigIterator: public ConfigStreamReader{
+        public:
+            /*! 
+             * \brief constructor 
+             * \param fname name of configure file
+             */
+	    ConfigIterator( const char *fname ):ConfigStreamReader(fi){
+	        fi.open(fname);
+		if( fi.fail() ){
+		    fprintf(stderr, "cannot open file %s\n", fname);
+		    utils::Error("cannot open file");
+		}
+		ConfigReaderBase::Init();
+            }
+            /*! \brief destructor */
+            ~ConfigIterator(){
+	        fi.close();
+            }
+	private:
+	    std::ifstream fi;
         };
     };
 };
