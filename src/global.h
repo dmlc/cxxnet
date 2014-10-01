6c31d4dba90b6eccdd3b01eda5feffc2ffa4ac49
tqchen <tianqi.tchen@gmail.com>
2014-09-30 20:43:29 -0700

    check all layers in, remove pad layer

---
+++ src/global.h
@@ -0,0 +1,30 @@
+#ifndef CXXNET_GLOBAL_H_
+/*!
+ * \file global.h
+ * \brief global configuration of cxxnet, this controls how cxxnet is compiled
+ * \author Tianqi Chen
+ */
+/*! \brief whether to adapt caffe layers */
+#ifndef CXXNET_ADAPT_CAFFE
+#define CXXNET_ADAPT_CAFFE 0
+#endif
+
+/*! \brief whether to reuse xgboost input sparse data structure */
+#ifndef CXXNET_ADAPT_XGBOOST
+#define CXXNET_ADAPT_XGBOOST 0
+#endif
+
+/*! \brief whether to use opencv support, without it, we will not be able to use load image iterarators */
+#ifndef CXXNET_USE_OPENCV
+#define CXXNET_USE_OPENCV 1
+#endif
+
+/*! \brief namespace of cxxnet */
+namespace cxxnet {
+typedef mshadow::cpu cpu;
+typedef mshadow::gpu gpu;
+typedef mshadow::index_t index_t;
+typedef mshadow::real_t real_t;
+};
+
+#endif  // CXXNET_GLOBAL_H_
---
+++ src/layer/softmax_layer-inl.hpp
@@ -0,0 +1,33 @@
+#ifndef LAYER_SOFTMAX_LAYER_INL_HPP_
+#define LAYER_SOFTMAX_LAYER_INL_HPP_
+
+#include "./layer.h"
+#include "mshadow/tensor.h"
+
+namespace cxxnet {
+namespace layer {
+/*! \brief softmax layer, do softmax transformation during forward */
+template<typename xpu>
+class SoftmaxLayer: public CommonLayerBase<xpu>{
+ public:
+  SoftmaxLayer(mshadow::Random<xpu> *p_rnd, Node<xpu> *p_in, Node<xpu> *p_out)
+      : CommonLayerBase<xpu>(p_rnd, p_in, p_out) {
+    utils::Check(p_in == p_out, "softmax layer must self loop e.g layer[1->1] = softmax");
+  }
+  virtual ~SoftmaxLayer(void) {}
+
+ protected:
+  virtual void InitLayer_(const Node<xpu> &node_in, Node<xpu> *pnode_out) {}
+  virtual void Forward_(bool is_train,
+                        Node<xpu> *pnode_in,
+                        Node<xpu> *pnode_out) {
+    mshadow::Softmax(pnode_out->mat(), pnode_out->mat() );
+  }
+  virtual void Backprop_(bool prop_grad,
+                         Node<xpu> *pnode_in,
+                         Node<xpu> *pnode_out) {
+  }
+};
+}  // namespace layer
+}  // namespace cxxnet
+#endif  // LAYER_SOFTMAX_LAYER_INL_HPP_
