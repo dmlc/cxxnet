18457e76a0510cd7e1bc7ef508b93ad214e40195
tqchen <tianqi.tchen@gmail.com>
2015-01-23 16:41:22 -0800

    new makefile

---
+++ make/config.mk
@@ -0,0 +1,48 @@
+#-----------------------------------------------------
+#  cxxnet: the configuration compile script
+#  
+#  This is the default configuration setup for cxxnet
+#  If you want to change configuration, you can:
+#     
+#  * copy this file to the root folder
+#  * modify the configuration you want
+#  * type make
+#----------------------------------------------------
+
+# choice of compiler
+export CC = gcc
+export CXX = g++
+export NVCC = nvcc
+
+# whether use CUDA during compile
+USE_CUDA = 1
+
+# add the path to CUDA libary to link and compile flag
+# if you have already add them to enviroment variable, leave it as NONE
+USE_CUDA_PATH = NONE
+
+# whether use opencv during compilation
+# you can disable it, however, you will not able to use
+# imbin iterator
+USE_OPENCV = 1
+
+# whether use CUDNN library
+USE_CUDNN = 0
+# add the path to CUDNN libary to link and compile flag
+# if you do not need that, or do not have that, leave it as NONE
+USE_CUDNN_PATH = NONE
+
+#
+# choose the version of blas you want to use
+# can be: mkl, blas, atlas, openblas
+USE_BLAS = mkl
+
+# whether compile with parameter server
+USE_DIST_PS = 0
+
+# the additional link flags you want to add
+ADD_LDFLAGS = NONE
+
+# the additional compile flags you want to add
+ADD_CFLAGS = NONE
+
--- src/dist_ps_server.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE
-#include <mshadow-ps/ps.h>
-#include <mshadow-ps/ps_dist-inl.h>
-#include "./global.h"
-// put it in PS namespace?
-PS::App* CreateServer(const std::string& conf) {
-  return new mshadow::ps::MShadowServer<cxxnet::real_t>(conf);
-}
