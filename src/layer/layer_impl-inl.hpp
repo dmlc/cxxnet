f235df1d08643e0be3bd8d97db60fc80d8f9479b
Bing Xu <antinucleon@gmail.com>
2015-01-29 19:14:22 -0800

    maxout, chpool is wrong

--- src/layer/layer_impl-inl.hpp
+++ src/layer/layer_impl-inl.hpp
@@ -22,6 +22,7 @@
 #include "./cudnn_pooling_layer-inl.hpp"
 #include "./xelu_layer-inl.hpp"
 #include "./insanity_layer-inl.hpp"
+#include "./maxout_layer-inl.hpp"
 #if CXXNET_USE_CAFFE_ADAPTOR
 #include "../plugin/caffe_adapter-inl.hpp"
 #endif
@@ -55,6 +56,7 @@ ILayer<xpu>* CreateLayer_(LayerType type,
     case kSplit: return new SplitLayer<xpu>();
     case kXelu: return new XeluLayer<xpu>();
     case kInsanity: return new InsanityLayer<xpu>(p_rnd);
+    case kMaxout: return new MaxoutLayer<mshadow::red::maximum, xpu>();
     #if CXXNET_USE_CAFFE_ADAPTOR
     case kCaffe: return new CaffeLayer<xpu>();
     #endif
