b7692e1fb242ffce1d7d7ab523439077e50d1a69
Bing Xu <antinucleon@gmail.com>
2015-02-10 00:30:38 -0800

    wtf fix

--- src/layer/prelu_layer-inl.hpp
+++ src/layer/prelu_layer-inl.hpp
@@ -6,6 +6,27 @@
 #include "./layer.h"
 #include "./op.h"
 
+namespace cxxnet {
+namespace op {
+struct mxelu {
+  MSHADOW_XINLINE static real_t Map(real_t a, real_t b) {
+    return a > 0.0f ? a : a * b;
+  }
+};
+
+struct mxelu_grad {
+  MSHADOW_XINLINE static real_t Map(real_t a, real_t b) {
+    return a > 0.0f ? 1 : b;
+  }
+};
+
+struct prelu_grad {
+  MSHADOW_XINLINE static real_t Map(real_t a) {
+    return a > 0.0f ? 0.0f : a;
+  }
+};
+} // namespace op
+} // namespace cxxnet
 
 namespace cxxnet {
 namespace layer {
@@ -15,20 +36,20 @@ class PReluLayer : public ILayer<xpu> {
  public:
   PReluLayer(mshadow::Random<xpu> *p_rnd) : prnd_(p_rnd) {
     // setup default value
-    init_slope_ = 3.0;
+    init_slope_ = 0.25f;
   }
   virtual void SetParam(const char *name, const char* val) {
     if (!strcmp(name, "init_slope")) init_slope_ = atof(val);
   }
   virtual void ApplyVisitor(typename ILayer<xpu>::IVisitor *pvisitor) {
-    pvisitor->Visit("wmat", slope_, gslope_);
-  } 
+    pvisitor->Visit("bias", slope_, gslope_);
+  }
   virtual void InitConnection(const std::vector<Node<xpu>*> &nodes_in,
                               const std::vector<Node<xpu>*> &nodes_out,
                               ConnectState<xpu> *p_cstate) {
     utils::Check(nodes_in.size() == 1 && nodes_out.size() == 1,
                  "PReluLayer: only support 1-1 connection");
-    
+
     nodes_out[0]->data.shape_ = nodes_in[0]->data.shape_;
     if (nodes_in[0]->data.size(1) == 1){
       // This is a fc layer
@@ -37,18 +58,20 @@ class PReluLayer : public ILayer<xpu> {
       // This is a conv layer
       channel_ = nodes_in[0]->data.size(1);
     }
+    in_shape_ = nodes_in[0]->data.shape_;
   }
   virtual void InitModel(void) {
     // resize to correct shape
     slope_.Resize(mshadow::Shape1(channel_));
     gslope_.Resize(mshadow::Shape1(channel_));
+    temp_.Resize(in_shape_);
     slope_ = init_slope_;
     gslope_ = 0.0;
   }
   virtual void SaveModel(utils::IStream &fo) const{
     slope_.SaveBinary(fo);
   }
-  virtual void LoadModel(utils::IStream &fi){  
+  virtual void LoadModel(utils::IStream &fi){
     slope_.LoadBinary(fi);
     // setup gradient weight
     gslope_.Resize(slope_.shape_);
@@ -57,6 +80,7 @@ class PReluLayer : public ILayer<xpu> {
   virtual void SetStream(mshadow::Stream<xpu> *stream) {
     slope_.set_stream(stream);
     gslope_.set_stream(stream);
+    temp_.set_stream(stream);
   }
   virtual void OnBatchSizeChanged(const std::vector<Node<xpu>*> &nodes_in,
                                   const std::vector<Node<xpu>*> &nodes_out,
@@ -70,10 +94,11 @@ class PReluLayer : public ILayer<xpu> {
     using namespace mshadow::expr;
     mshadow::Tensor<xpu, 4> &in = nodes_in[0]->data;
     mshadow::Tensor<xpu, 4> &out = nodes_out[0]->data;
+    mshadow::Copy(temp_, in, out.stream_);
     if (in.size(1) != 1){
-      in = F<op::xelu>(in, broadcast<1>(slope_, in.shape_));
+      in = F<op::mxelu>(in, broadcast<1>(slope_, in.shape_));
     } else {
-      in = F<op::xelu>(in, broadcast<3>(slope_, in.shape_));
+      in = F<op::mxelu>(in, broadcast<3>(slope_, in.shape_));
     }
     mshadow::Copy(out, in, out.stream_);
   }
@@ -84,15 +109,16 @@ class PReluLayer : public ILayer<xpu> {
     using namespace mshadow::expr;
     mshadow::Tensor<xpu, 4> &in = nodes_in[0]->data;
     mshadow::Tensor<xpu, 4> &out = nodes_out[0]->data;
+    temp_ = F<op::prelu_grad>(temp_) * out;
     if (in.size(1) != 1){
-      gslope_ += sumall_except_dim<1>(out);
+      gslope_ += sumall_except_dim<1>(temp_);
       if (prop_grad){
-        in = F<op::xelu_grad>(in, broadcast<1>(slope_, in.shape_)) * out;
+        in = F<op::mxelu_grad>(in, broadcast<1>(slope_, in.shape_)) * out;
       }
     } else {
-      gslope_ += sumall_except_dim<3>(out);
+      gslope_ += sumall_except_dim<3>(temp_);
       if (prop_grad){
-        in = F<op::xelu_grad>(in, broadcast<3>(slope_, in.shape_)) * out;
+        in = F<op::mxelu_grad>(in, broadcast<3>(slope_, in.shape_)) * out;
       }
     }
   }
@@ -108,7 +134,9 @@ class PReluLayer : public ILayer<xpu> {
   mshadow::TensorContainer<xpu,1> slope_;
   /*! \brief gradient of slope */
   mshadow::TensorContainer<xpu,1> gslope_;
-  
+  mshadow::TensorContainer<xpu,4> temp_;
+  mshadow::Shape<4> in_shape_;
+
 };  // class PReluLayer
 
 } // namespace layer
