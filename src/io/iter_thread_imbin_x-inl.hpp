3128e5718733e145a29a396308e17222592b9c1d
tqchen <tianqi.tchen@gmail.com>
2015-02-26 10:27:14 -0800

    new version of imbinx

--- src/io/iter_thread_imbin_x-inl.hpp
+++ src/io/iter_thread_imbin_x-inl.hpp
@@ -19,7 +19,8 @@ class ThreadImagePageIteratorX: public IIterator<DataInst> {
 public:
   ThreadImagePageIteratorX(void) {
     silent_ = 0;
-    itr.SetParam("buffer_size", "2048");
+    itrpage.SetParam("buffer_size", "2");
+    itrimg.SetParam("buffer_size", "256");
     img_conf_prefix_ = "";
     dist_num_worker_ = 0;
     dist_worker_rank_ = 0;
@@ -50,7 +51,8 @@ public:
       dist_worker_rank_ = atoi(val);
     }
     if (!strcmp(name, "silent")) silent_ = atoi(val);
-    itr.get_factory().SetParam(name, val);
+    itrpage.get_factory().SetParam(name, val);
+    itrimg.get_factory().SetParam(name, val);
   }
   virtual void Init(void) {
     this->ParseImageConf();
@@ -65,16 +67,24 @@ public:
     }
     utils::Check(path_imgbin_.size() == path_imglst_.size(),
                  "List/Bin number not consist");
-    itr.get_factory().path_imgbin = path_imgbin_;
-    itr.get_factory().path_imglst = path_imglst_;
-    itr.Init();
+    itrpage.get_factory().path_imgbin = path_imgbin_;
+    itrpage.get_factory().path_imglst = path_imglst_;
+    itrpage.Init();
+    itrimg.get_factory().itrpage = &itrpage;
     this->BeforeFirst();
   }
   virtual void BeforeFirst(void) {
-    itr.BeforeFirst();
+    itrimg.BeforeFirst();
   }
   virtual bool Next(void) {
-    return itr.Next(out_);
+    if (itrimg.Next(outimg_)) {
+      out_.index = outimg_->inst_index;
+      out_.label = outimg_->label;
+      out_.data = outimg_->img;
+      return true;
+    } else {
+      return false;
+    }
   }
   virtual const DataInst &Value(void) const {
     return out_;
@@ -129,84 +139,36 @@ public:
   }
 
 private:
-  struct InstEntry {
-    // insance index
-    unsigned inst_index;
-    // label of each instance
-    mshadow::TensorContainer<cpu, 1> label;
-    // image data
-    mshadow::TensorContainer<cpu, 3> img;
-    // instance entry
-    InstEntry() : label(false), img(false) {
-      
-    }
-  };
-  struct Factory {
-  public:
-    // put everything in cache entry
-    struct CacheEntry {
-      // insance index
-      unsigned inst_index;
-      // label of each instance
-      mshadow::TensorContainer<cpu, 1> label;
-      // image data
-      mshadow::TensorContainer<cpu, 3, unsigned char> img;
-      CacheEntry() : label(false), img(false) {}
-      CacheEntry(int label_width,
-                 mshadow::Shape<3> imshape)
-          : label(false), img(false) {
-        label.Resize(mshadow::Shape1(label_width));
-        img.Resize(imshape);
-      }
-    };
-    // file stream for binary page
-    utils::StdFile fi;
-    // page ptr
+  // load page data structure
+  struct PageEntry {
     utils::BinaryPage page;
+    std::vector<float> labels;
+    std::vector<unsigned> inst_index;
+  };
+  // factory to load page
+  struct PageFactory {
+   public:
     // list of bin path
     std::vector<std::string> path_imgbin;
     // list of img list path
     std::vector<std::string> path_imglst;
-    // seq of list index
-    std::vector<int> list_order;
-    // seq of inst index
-    std::vector<int> inst_order;
-    // jpeg decoders
-    std::vector<utils::JpegDecoder*> decoders;
-    // decoded data
-    std::vector<CacheEntry> entry;
-    // image shape
-    mshadow::Shape<3> data_shape;
-    /*! \brief label-width */
-    int label_width_;
-    // pointer for each list
-    size_t list_ptr;
-    // id for data
-    int data_ptr;
-    // number of decoding thread
-    int nthread;
-    // file ptr for list
-    FILE *fplist;
-    // shuffle
-    int shuffle;
-
-  public:
-    Factory(void) {
-      nthread = 3;
-      label_width_ = 1;
+    // constructor
+    PageFactory(void) {
+      label_width = 1;
       list_ptr = 0;
-      data_ptr = 0;
       fplist = NULL;
       shuffle = 0;
       rnd.Seed(kRandMagic); 
-      // setup decoders
-      for (int i = 0; i < nthread; ++i) {
-        decoders.push_back(new utils::JpegDecoder());
-      }
     }
-    ~Factory(void) {
-      for (int  i = 0; i < nthread; ++i) {
-        delete decoders[i];       
+    inline void SetParam(const char *name, const char *val) {
+      if (!strcmp(name, "label_width")) {
+        label_width = atoi(val);
+      }
+      if (!strcmp(name, "shuffle")) {
+        shuffle = atoi(val);
+      }
+      if (!strcmp(name, "seed_data")) {
+        rnd.Seed(atoi(val) + kRandMagic); 
       }
     }
     inline bool Init(void) {
@@ -221,111 +183,9 @@ private:
       list_ptr = 0;
       fi.Open(path_imgbin[list_order[0]].c_str(), "rb");
       fplist = utils::FopenCheck(path_imglst[list_order[0]].c_str(), "r");
-      utils::Check(this->FillBuffer(), "ImageIterator: first bin must be valid");
-      // after init, we will know the data shape
-      data_shape = entry[0].img.shape_;
       return true;
     }
-    inline void SetParam(const char *name, const char *val) {
-      if (!strcmp(name, "label_width")) {
-        label_width_ = atoi(val);
-      }
-      if (!strcmp(name, "shuffle")) {
-        shuffle = atoi(val);
-      }
-      if (!strcmp(name, "seed_data")) {
-        rnd.Seed(atoi(val) + kRandMagic); 
-      }
-    }
-    inline bool FillBuffer(void) {
-      bool res = page.Load(fi);
-      if (!res) return res;
-      // always keep entry to maximum size to avoid re-allocation
-      entry.resize(std::max(entry.size(),
-                            static_cast<size_t>(page.Size())));
-      if (shuffle != 0) {
-        inst_order.resize(page.Size());
-        for (int i = 0; i < page.Size(); ++i) {
-          inst_order[i] = i;
-        }
-        rnd.Shuffle(inst_order);
-      }
-      // omp here
-      #pragma omp parallel for num_threads(nthread)
-      for (int i = 0; i < page.Size(); ++i) {
-        utils::BinaryPage::Obj obj = (shuffle != 0) ? page[inst_order[i]] : page[i];
-        const int tid = omp_get_thread_num();
-        decoders[tid]->Decode(static_cast<unsigned char*>(obj.dptr),
-                              obj.sz,
-                              &entry[i].img);
-      }
-      for (int i = 0; i < page.Size(); ++i) {
-        utils::Check(fscanf(fplist, "%u", &entry[i].inst_index) == 1,
-                      "invalid list format");
-        entry[i].label.Resize(mshadow::Shape1(label_width_));
-        for (int j = 0; j < label_width_; ++j) {
-          float tmp;
-          utils::Check(fscanf(fplist, "%f", &tmp) == 1,
-                     "ImageList format:label_width=%u but only have %d labels per line",
-                     label_width_, j);
-          entry[i].label[j] = tmp;
-        }
-        utils::Assert(fscanf(fplist, "%*[^\n]\n") == 0, "ignore");
-      }
-      data_ptr = 0;
-      return true;
-    }
-    inline DataInst Create(void) {
-      DataInst a;
-      a.data.shape_ = mshadow::Shape3(3, data_shape[1], data_shape[0]);
-      a.label.shape_ = mshadow::Shape1(label_width_);
-      mshadow::AllocSpace(&a.data, false);
-      mshadow::AllocSpace(&a.label, false);
-      return a;
-    }
-    inline void FreeSpace(DataInst &a) {
-      mshadow::FreeSpace(&a.data);
-      mshadow::FreeSpace(&a.label);
-    }
-    inline bool LoadNext(DataInst &val) {
-      while (true) {
-        if (data_ptr >= page.Size()) {
-          if (!this->FillBuffer()) {
-            list_ptr += 1;
-            if (list_ptr >= list_order.size()) return false;
-            fi.Close();
-            fi.Open(path_imgbin[list_order[list_ptr]].c_str(), "rb");
-            if (fplist != NULL) fclose(fplist);
-            fplist = utils::FopenCheck(path_imglst[list_order[list_ptr]].c_str(), "r");
-          }
-        } else {
-          using namespace mshadow::expr;
-          val.index = entry[data_ptr].inst_index;
-          mshadow::Copy(val.label, entry[data_ptr].label);
-          utils::Check(val.data.shape_[2] == entry[data_ptr].img.shape_[0] &&
-                       val.data.shape_[1] == entry[data_ptr].img.shape_[1],
-                       "all the images in the bin must have same shape");
-          if (entry[data_ptr].img.shape_[2] == 3) {
-            val.data = tcast<real_t>(swapaxis<2, 0>(entry[data_ptr].img));
-          } else {
-            for (unsigned int i = 0; i < entry[data_ptr].img.size(1); ++i) {
-              for (unsigned int j = 0; j < entry[data_ptr].img.size(0); ++j) {
-                val.data[0][i][j] = static_cast<real_t>(entry[data_ptr].img[j][i][0]);
-                val.data[1][i][j] = static_cast<real_t>(entry[data_ptr].img[j][i][0]);
-                val.data[2][i][j] = static_cast<real_t>(entry[data_ptr].img[j][i][0]);
-              }
-            }
-          }
-          data_ptr += 1;
-          return true;
-        }
-      }
-    }
-    inline void Destroy() {
-      fi.Close();
-      if (fplist != NULL) fclose(fplist);
-    }
-    inline void BeforeFirst() {
+    inline void BeforeFirst(void) {
       list_ptr = 0;
       if (path_imgbin.size() == 1) {
         fi.Seek(0);
@@ -339,16 +199,192 @@ private:
         if (fplist != NULL) fclose(fplist);
         fplist = utils::FopenCheck(path_imglst[list_order[0]].c_str(), "r");
       }
-      utils::Check(this->FillBuffer(), "the first bin was empty");
     }
-
+    inline PageEntry *Create(void) {
+      return new PageEntry();
+    }
+    inline bool LoadNext(PageEntry *&a) {
+      while (true) {
+        if (a->page.Load(fi)) {
+          a->labels.resize(a->page.Size() * label_width);
+          a->inst_index.resize(a->page.Size());
+          for (int i = 0; i < a->page.Size(); ++i) {
+            utils::Check(fscanf(fplist, "%u", &(a->inst_index[i])) == 1,
+                         "invalid list format");
+            for (int j = 0; j < label_width; ++j) {
+              utils::Check(fscanf(fplist, "%f", &(a->labels[i * label_width + j])) == 1,
+                           "ImageList format:label_width=%u but only have %d labels per line",
+                           label_width, j);
+              
+            }
+            utils::Assert(fscanf(fplist, "%*[^\n]\n") == 0, "ignore");
+          }          
+        } else {
+          list_ptr += 1;
+          if (list_ptr >= list_order.size()) return false;
+          fi.Close();
+          fi.Open(path_imgbin[list_order[list_ptr]].c_str(), "rb");
+          if (fplist != NULL) fclose(fplist);
+          fplist = utils::FopenCheck(path_imglst[list_order[list_ptr]].c_str(), "r");
+        }
+      }
+    }
+    inline void FreeSpace(PageEntry *&a) {
+      delete a;
+    }
+    inline void Destroy() {
+      fi.Close();
+      if (fplist != NULL) fclose(fplist);
+    }
+   
    private:
+    // file stream for binary page
+    utils::StdFile fi;
+    // seq of list index
+    std::vector<size_t> list_order;
+    /*! \brief label-width */
+    int label_width;
+    // pointer for each list
+    size_t list_ptr;
+    // file ptr for list
+    FILE *fplist;
+    // shuffle
+    int shuffle;
+    // random sampler
     utils::RandomSampler rnd;
+    // magic seed number for random sampler
+    static const int kRandMagic = 121;    
+  };  
+  // put everything in inst entry
+  struct ImageEntry {
+    // insance index
+    unsigned inst_index;
+    // label of each instance
+    mshadow::TensorContainer<cpu, 1> label;
+    // image data
+    mshadow::TensorContainer<cpu, 3> img;
+    ImageEntry() : label(false), img(false) {}
+  };
+  struct ImageFactory {
+  public:
+    // page iterator
+    utils::ThreadBuffer<PageEntry*, PageFactory> *itrpage;
+    // constructor
+    ImageFactory(void) {
+      label_width = 1;
+      data_ptr = 0;
+      shuffle = 0;
+      end_of_data = false;
+      page = NULL;
+      rnd.Seed(kRandMagic);       
+    }
+    inline void SetParam(const char *name, const char *val) {
+      if (!strcmp(name, "label_width")) {
+        label_width = atoi(val);
+      }
+      if (!strcmp(name, "shuffle")) {
+        shuffle = atoi(val);
+      }
+      if (!strcmp(name, "seed_data")) {
+        rnd.Seed(atoi(val) + kRandMagic); 
+      }
+    }
+    inline bool Init(void) {
+      return true;
+    }
+    inline ImageEntry *Create(void) {
+      return new ImageEntry();
+    }
+    inline void FreeSpace(ImageEntry *&a) {
+      delete a;
+    }
+    inline bool LoadNext(ImageEntry *&val) {
+      if (end_of_data) return false;
+      while (true) {
+        if (page == NULL || data_ptr >= page->page.Size()) {
+          if (!itrpage->Next(page)) {
+            end_of_data = true; return false;
+          }
+          data_ptr = 0;
+          inst_order.resize(page->page.Size());
+          for (int i = 0; i < page->page.Size(); ++i) {
+            inst_order[i] = i;
+          }
+          if (shuffle != 0) {
+            rnd.Shuffle(inst_order);
+          }
+        } else {
+          const int idx = inst_order[data_ptr];
+          utils::BinaryPage::Obj obj = page->page[idx];
+          decoder.Decode(static_cast<unsigned char*>(obj.dptr),
+                         obj.sz, &img);
+          val->img.Resize(mshadow::Shape3(3, img.size(0), img.size(1)));         
+          // assign image
+          if (img.size(0) == 3) {
+            mshadow::Tensor<cpu, 3> dst = val->img;
+            for (index_t i = 0; i < img.size(0); ++i) {
+              for (index_t j = 0; j < img.size(1); ++j) {
+                for (index_t k = 0; k < 3; ++k) {
+                  dst[k][i][j] = static_cast<real_t>(img[i][j][k]);                
+                }
+              }
+            }
+          } else {
+            mshadow::Tensor<cpu, 3> dst = val->img;
+            for (index_t i = 0; i < img.size(0); ++i) {
+              for (index_t j = 0; j < img.size(1); ++j) {
+                real_t s = static_cast<real_t>(img[i][j][0]);
+                dst[0][i][j] = s;
+                dst[1][i][j] = s;
+                dst[2][i][j] = s;
+              }
+            }
+          }
+          val->label.Resize(mshadow::Shape1(label_width));
+          for (int j = 0; j < label_width; ++j) {
+            val->label[j] = page->labels[idx * label_width + j];
+          }
+          val->inst_index = page->inst_index[idx];
+          data_ptr += 1;
+          return true;
+        }
+      }
+    }
+    inline void Destroy() {}
+    inline void BeforeFirst() {
+      itrpage->BeforeFirst();
+      end_of_data = false;
+      page = NULL;
+      data_ptr = 0;
+    }
+   private:
+    // mark end of data
+    bool end_of_data;
+    // current page
+    PageEntry *page;    
+    // seq of inst index
+    std::vector<int> inst_order;
+    // jpeg decoder
+    utils::JpegDecoder decoder;    
+    // id for data
+    int data_ptr;
+    // shuffle
+    int shuffle;
+    // label_width
+    int label_width;
+    // image
+    mshadow::TensorContainer<cpu, 3, unsigned char> img;
+    // random number generator
+    utils::RandomSampler rnd;
+    // magic number
     static const int kRandMagic = 111;
   };
 
 protected:
-  utils::ThreadBuffer<DataInst, Factory> itr;
+  /*! \brief output data */
+  ImageEntry *outimg_;
+  utils::ThreadBuffer<PageEntry*, PageFactory> itrpage;
+  utils::ThreadBuffer<ImageEntry*, ImageFactory> itrimg;
 }; // class ThreadImagePageIterator
 }; // namespace cxxnet
 #endif
