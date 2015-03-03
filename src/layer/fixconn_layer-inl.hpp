946482ef285497f0823ad677f689b37f4222a25d
Bing Xu <antinucleon@gmail.com>
2015-03-03 14:45:54 -0800

    fix stream

--- src/layer/fixconn_layer-inl.hpp
+++ src/layer/fixconn_layer-inl.hpp
@@ -15,11 +15,12 @@ class FixConnectLayer : public ILayer<xpu> {
  public:
   FixConnectLayer(void) {
     fname_weight_ = "NULL";
-  }  
+    init = false;
+  }
   virtual void SetParam(const char *name, const char* val) {
     param_.SetParam(name, val);
     if (!strcmp(name, "fixconn_weight")) fname_weight_ = val;
-  }  
+  }
   virtual void SetStream(mshadow::Stream<xpu> *stream) {
     wmat_.set_stream(stream);
   }
@@ -29,13 +30,14 @@ class FixConnectLayer : public ILayer<xpu> {
     utils::Check(nodes_in.size() == 1 && nodes_out.size() == 1,
                  "FixConnLayer: Layer only support 1-1 connection");
     utils::Check(nodes_in[0]->is_mat(), "FixConnLayer: input need to be a matrix");
-    utils::Check(param_.num_hidden > 0, "FixConncLayer: must set nhidden correctly");    
+    utils::Check(param_.num_hidden > 0, "FixConncLayer: must set nhidden correctly");
     // we change matrix convention
     nodes_out[0]->data.shape_ =
         mshadow::Shape4(nodes_in[0]->data.size(0), 1, 1, param_.num_hidden);
     wmat_.Resize(mshadow::Shape2(param_.num_hidden, nodes_in[0]->mat().size(1)));
     utils::Check(fname_weight_ != "NULL", "FixConnLayer: must specify fixconn_weight");
-    mshadow::TensorContainer<cpu, 2> tmp(false);
+    // mshadow::TensorContainer<cpu, 2> tmp(false);
+    tmp.set_pad(false);
     tmp.Resize(wmat_.shape_); tmp = 0.0f;
     FILE *fi = utils::FopenCheck(fname_weight_.c_str(), "r");
     unsigned nrow, ncol, nonzero;
@@ -53,16 +55,20 @@ class FixConnectLayer : public ILayer<xpu> {
       tmp[x][y] = value;
     }
     fclose(fi);
-    mshadow::Stream<xpu> *stream = wmat_.stream_;
-    mshadow::Copy(wmat_, tmp, stream);
+    //mshadow::Stream<xpu> *stream = wmat_.stream_;
+    // mshadow::Copy(wmat_, tmp, stream);
     // must wait till copy end, otherwise tmp will be de-allocated
-    if (stream != NULL) stream->Wait();
+    // if (stream != NULL) stream->Wait();
   }
   virtual void Forward(bool is_train,
                        const std::vector<Node<xpu>*> &nodes_in,
                        const std::vector<Node<xpu>*> &nodes_out,
                        ConnectState<xpu> *p_cstate) {
     using namespace mshadow::expr;
+    if (!init) {
+      mshadow::Copy(wmat_, tmp, wmat_.stream_);
+      init = true;
+    }
     nodes_out[0]->mat() = dot(nodes_in[0]->mat(), wmat_.T());
   }
   virtual void Backprop(bool prop_grad,
@@ -78,9 +84,12 @@ class FixConnectLayer : public ILayer<xpu> {
   /*! \brief name to the weight */
   std::string fname_weight_;
   /*! \brief parameters that potentially be useful */
-  LayerParam param_;    
+  LayerParam param_;
   /*! \brief weight matrix */
-  mshadow::TensorContainer<xpu, 2> wmat_;  
+  mshadow::TensorContainer<xpu, 2> wmat_;
+  /*! \brief temp weight */
+  mshadow::TensorContainer<cpu, 2> tmp;
+  bool init;
 };
 }  // namespace layer
 }  // namespace cxxnet
