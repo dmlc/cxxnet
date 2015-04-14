c7b9d294ffff739183a35d095036d5acd826b7c8
Bing Xu <antinucleon@gmail.com>
2015-04-14 10:30:18 -0600

    fix check

--- src/layer/cudnn_convolution_layer-inl.hpp
+++ src/layer/cudnn_convolution_layer-inl.hpp
@@ -25,12 +25,12 @@ class CuDNNConvolutionLayer<gpu> : public ConvolutionLayer<gpu> {
   };
 #if CXXNET_USE_CUDNN == 1
   virtual ~CuDNNConvolutionLayer() {
-    CUDA_CHECK(cudnnDestroyTensorDescriptor(in_desc_));
-    CUDA_CHECK(cudnnDestroyTensorDescriptor(out_desc_));
-    CUDA_CHECK(cudnnDestroyTensorDescriptor(bias_desc_));
-    CUDA_CHECK(cudnnDestroyFilterDescriptor(filter_desc_));
-    CUDA_CHECK(cudnnDestroyConvolutionDescriptor(conv_desc_));
-    CUDA_CHECK(cudnnDestroy(handle_));
+    utils::Check(cudnnDestroyTensorDescriptor(in_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnDestroyTensorDescriptor(out_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnDestroyTensorDescriptor(bias_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnDestroyFilterDescriptor(filter_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnDestroyConvolutionDescriptor(conv_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnDestroy(handle_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
   };
   virtual void InitConnection(const std::vector<Node<gpu>*> &nodes_in,
                               const std::vector<Node<gpu>*> &nodes_out,
@@ -62,46 +62,46 @@ class CuDNNConvolutionLayer<gpu> : public ConvolutionLayer<gpu> {
         algo_ = CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM;
       }
       temp_.set_stream(nodes_out[0]->data.stream_);
-      CUDA_CHECK(cudnnSetStream(handle_, nodes_out[0]->data.stream_->stream_));
-      CUDA_CHECK(cudnnSetFilter4dDescriptor(filter_desc_, dtype_,
+      utils::Check(cudnnSetStream(handle_, nodes_out[0]->data.stream_->stream_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+      utils::Check(cudnnSetFilter4dDescriptor(filter_desc_, dtype_,
                                             Parent::param_.num_channel,
                                             Parent::param_.num_input_channel,
                                             Parent::param_.kernel_height,
-                                            Parent::param_.kernel_width));
-      CUDA_CHECK(cudnnSetConvolution2dDescriptor(conv_desc_,
+                                            Parent::param_.kernel_width) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+      utils::Check(cudnnSetConvolution2dDescriptor(conv_desc_,
                                                  Parent::param_.pad_y,
                                                  Parent::param_.pad_x,
                                                  Parent::param_.stride,
                                                  Parent::param_.stride, 1, 1,
-                                                 CUDNN_CROSS_CORRELATION));
+                                                 CUDNN_CROSS_CORRELATION) == CUDNN_STATUS_SUCCESS, "cudnn failed");
       mshadow::Tensor<gpu, 4, float> &in = nodes_in[0]->data;
       mshadow::Tensor<gpu, 4, float> &out = nodes_out[0]->data;
-      CUDA_CHECK(cudnnSetTensor4dDescriptor(in_desc_, CUDNN_TENSOR_NCHW, dtype_,
+      utils::Check(cudnnSetTensor4dDescriptor(in_desc_, CUDNN_TENSOR_NCHW, dtype_,
                                             in.shape_[0], in.shape_[1],
-                                            in.shape_[2], in.shape_[3]));
-      CUDA_CHECK(cudnnSetTensor4dDescriptor(out_desc_, CUDNN_TENSOR_NCHW, dtype_,
+                                            in.shape_[2], in.shape_[3]) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+      utils::Check(cudnnSetTensor4dDescriptor(out_desc_, CUDNN_TENSOR_NCHW, dtype_,
                                             out.shape_[0], out.shape_[1],
-                                            out.shape_[2], out.shape_[3]));
-      CUDA_CHECK(cudnnSetTensor4dDescriptor(bias_desc_, CUDNN_TENSOR_NCHW, dtype_,
-                                            1, Parent::bias_.shape_[0], 1, 1));
-      CUDA_CHECK(cudnnGetConvolutionForwardWorkspaceSize(handle_, in_desc_,
+                                            out.shape_[2], out.shape_[3]) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+      utils::Check(cudnnSetTensor4dDescriptor(bias_desc_, CUDNN_TENSOR_NCHW, dtype_,
+                                            1, Parent::bias_.shape_[0], 1, 1) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+      utils::Check(cudnnGetConvolutionForwardWorkspaceSize(handle_, in_desc_,
                                                          filter_desc_, conv_desc_,
                                                          out_desc_, algo_,
-                                                         &workspace_size_));
+                                                         &workspace_size_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
       temp_.Resize(mshadow::Shape1(workspace_size_ / sizeof(float) + 1), 0.0f);
     }
     utils::Assert(nodes_in[0]->data.CheckContiguous(), "contiguous in conv");
     utils::Assert(nodes_out[0]->data.CheckContiguous(), "contiguous in conv");
-    CUDA_CHECK(cudnnConvolutionForward(handle_, &alpha,
+    utils::Check(cudnnConvolutionForward(handle_, &alpha,
                                        in_desc_, nodes_in[0]->data.dptr_,
                                        filter_desc_, Parent::wmat_.dptr_,
                                        conv_desc_, algo_, temp_.dptr_, workspace_size_, &beta,
-                                       out_desc_, nodes_out[0]->data.dptr_));
+                                       out_desc_, nodes_out[0]->data.dptr_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
     if (Parent::param_.no_bias == 0) {
       beta = 1.0f;
-      CUDA_CHECK(cudnnAddTensor(handle_, CUDNN_ADD_SAME_C, &alpha,
+      utils::Check(cudnnAddTensor(handle_, CUDNN_ADD_SAME_C, &alpha,
                                 bias_desc_, Parent::bias_.dptr_, &beta,
-                                out_desc_, nodes_out[0]->data.dptr_));
+                                out_desc_, nodes_out[0]->data.dptr_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
     }
 
 
@@ -113,33 +113,33 @@ class CuDNNConvolutionLayer<gpu> : public ConvolutionLayer<gpu> {
     float alpha = 1.0f;
     float beta = 0.0f;
     if (Parent::param_.no_bias == 0) {
-      CUDA_CHECK(cudnnConvolutionBackwardBias(handle_, &alpha,
+      utils::Check(cudnnConvolutionBackwardBias(handle_, &alpha,
                                               out_desc_, nodes_out[0]->data.dptr_,
                                               &beta,
-                                              bias_desc_, Parent::gbias_.dptr_));
+                                              bias_desc_, Parent::gbias_.dptr_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
     }
-    CUDA_CHECK(cudnnConvolutionBackwardFilter(handle_, &alpha,
+    utils::Check(cudnnConvolutionBackwardFilter(handle_, &alpha,
                                               in_desc_, nodes_in[0]->data.dptr_,
                                               out_desc_, nodes_out[0]->data.dptr_,
                                               conv_desc_, &beta,
-                                              filter_desc_, Parent::gwmat_.dptr_));
-    CUDA_CHECK(cudnnConvolutionBackwardData(handle_, &alpha,
+                                              filter_desc_, Parent::gwmat_.dptr_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnConvolutionBackwardData(handle_, &alpha,
                                             filter_desc_, Parent::wmat_.dptr_,
                                             out_desc_, nodes_out[0]->data.dptr_,
                                             conv_desc_, &beta,
-                                            in_desc_, nodes_in[0]->data.dptr_));
+                                            in_desc_, nodes_in[0]->data.dptr_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
   }
  private:
   inline void InitCuDNN() {
     init_cudnn_ = false;
     dtype_ = CUDNN_DATA_FLOAT;
     algo_ = CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM;
-    CUDA_CHECK(cudnnCreate(&handle_));
-    CUDA_CHECK(cudnnCreateTensorDescriptor(&in_desc_));
-    CUDA_CHECK(cudnnCreateTensorDescriptor(&out_desc_));
-    CUDA_CHECK(cudnnCreateTensorDescriptor(&bias_desc_));
-    CUDA_CHECK(cudnnCreateFilterDescriptor(&filter_desc_));
-    CUDA_CHECK(cudnnCreateConvolutionDescriptor(&conv_desc_));
+    utils::Check(cudnnCreate(&handle_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnCreateTensorDescriptor(&in_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnCreateTensorDescriptor(&out_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnCreateTensorDescriptor(&bias_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnCreateFilterDescriptor(&filter_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
+    utils::Check(cudnnCreateConvolutionDescriptor(&conv_desc_) == CUDNN_STATUS_SUCCESS, "cudnn failed");
   }
   /*! \brief cuDNN init status */
   bool init_cudnn_;
