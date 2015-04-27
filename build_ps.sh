7d5693d17da763da8e6651df117da10c263a72fc
Ubuntu <ubuntu@node1.maas>
2015-04-27 02:57:24 +0000

    script to build with ps

---
+++ build_ps.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ ! -d ps-lite ]; then
+    git clone https://github.com/dmlc/ps-lite
+    echo "Install third-party libraries for PS"
+    ./ps-lite/make/install_third.sh 1>install_ps.log
+    echo "Compile PS"
+    make -j 4 -C ps-lite
+fi
+
+if [ ! -d mshadow ]; then
+    git clone https://github.com/dmlc/mshadow.git
+fi
+
+if [ ! -f config.mk ]; then
+    echo "Use the default config.m"
+    cp make/config.mk config.mk
+fi
+
+sed -i 's/USE_DIST_PS.*/USE_DIST_PS = 1/' config.mk
+sed -i 's/PS_PATH.*/PS_PATH = .\/ps-lite/' config.mk
+
+make -j4
+# make $1
